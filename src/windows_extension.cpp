f5d3b3270959615d318bd891549e5f16a0bb87d7
zokker13 <zokker13@posteo.de>
2017-02-25 13:31:29 +0100

    Remove legacy code

--- src/windows_extension.cpp
+++ src/windows_extension.cpp
@@ -1,19 +1,5 @@
 #include "windows_extension.hpp"
 
-void _ExReadProcessMemory(const Nan::FunctionCallbackInfo<v8::Value>& info)
-{
-  int value = 0;
-  SIZE_T bytesRead = 0;
-  BOOL success = ReadProcessMemory(HANDLE(info[0]->IntegerValue()), (void *)info[1]->IntegerValue(), &value, (SIZE_T)info[2]->IntegerValue(), &bytesRead);
-
-  cout << "Success: " << success << endl;
-  cout << "Value: " << value << endl;
-  cout << "Bytes read: " << bytesRead << endl;
-
-
-  info.GetReturnValue().Set(Nan::New((int)value)->ToInteger());
-}
-
 NAN_METHOD(NanWin32OpenProcess)
 {
   Callback *cb = new Callback(info[3].As<Function>());
